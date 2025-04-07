dfeea1b2b775919897a82dadd483582b32e80e9d
cantokhay <cantokhay@gmail.com>
2025-04-07 11:32:17 +0300

    End of 22

---
+++ UI/APIBootcamp.UI/ViewComponents/Default/_FeatureDefaultPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APIBootcamp.UI.ViewComponents.Default
+{
+    public class _FeatureDefaultPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
