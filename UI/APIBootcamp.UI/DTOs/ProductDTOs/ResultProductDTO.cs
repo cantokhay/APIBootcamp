318e5924deee1888b99d7ad5752858689c8a5de7
cantokhay <cantokhay@gmail.com>
2025-07-28 21:19:55 +0300

    End of 26

---
+++ UI/APIBootcamp.UI/DTOs/ProductDTOs/ResultProductDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace APIBootcamp.UI.DTOs.ProductDTOs
+{
+    public class ResultProductDTO
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductImageURL { get; set; }
+        //public DataStatus DataStatus { get; set; }
+        //public DateTime CreatedDate { get; set; }
+        //public DateTime? ModifiedDate { get; set; }
+        //public DateTime? DeletedDate { get; set; }
+
+        public int CategoryId { get; set; }
+        //public Category Category { get; set; }
+    }
+}
---
+++ UI/APIBootcamp.UI/ViewComponents/Default/_MenuProductDefaultPartialComponent.cs
@@ -0,0 +1,29 @@
+﻿using APIBootcamp.UI.DTOs.ProductDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace APIBootcamp.UI.ViewComponents.Default
+{
+    public class _MenuProductDefaultPartialComponent : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _MenuProductDefaultPartialComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var response = await client.GetAsync("https://localhost:7243/api/Products");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
