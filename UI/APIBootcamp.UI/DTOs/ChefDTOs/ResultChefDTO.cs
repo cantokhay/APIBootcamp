92e72de783d499e60c1071c19aa418668f3ed04e
cantokhay <cantokhay@gmail.com>
2025-07-30 16:39:04 +0300

    End of 30

---
+++ UI/APIBootcamp.UI/DTOs/ChefDTOs/ResultChefDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace APIBootcamp.UI.DTOs.ChefDTOs
+{
+    public class ResultChefDTO
+    {
+        public int Id { get; set; }
+        public string ChefFullName { get; set; }
+        public string ChefTitle { get; set; }
+        public string ChefComment { get; set; }
+        public string ChefImageURL { get; set; }
+        //public DataStatus DataStatus { get; set; }
+        //public DateTime CreatedDate { get; set; }
+        //public DateTime? ModifiedDate { get; set; }
+        //public DateTime? DeletedDate { get; set; }
+    }
+}
---
+++ UI/APIBootcamp.UI/ViewComponents/Default/_ChefDefaultPartialComponent.cs
@@ -0,0 +1,28 @@
+﻿using APIBootcamp.UI.DTOs.ChefDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace APIBootcamp.UI.ViewComponents.Default
+{
+    public class _ChefDefaultPartialComponent : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _ChefDefaultPartialComponent(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient("Default");
+            var response = await client.GetAsync("https://localhost:7243/api/Chefs/");
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultChefDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
